91f7b3a4a0ccae77aaefd4c574bdda352c421ec5
YaKashubo <kashubo.work@mail.ru>
2023-05-31 21:35:59 +0300

    homework5 done

--- 2023.04.23homework5/KNN.py
+++ 2023.04.23homework5/KNN.py
@@ -1,54 +1,53 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-l = 10 ** 5
-arr = []
-x = []
-y = []
-dataset = {}
+# x, y = make_classification(n_samples=10 ** 5, n_features=2,n_redundant=0,n_clusters_per_class=1,n_classes=4, class_sep=1, )
+# plt.scatter(x[:, 0], x[:, 1], c=y,s=3)
+# plt.show()
+# f1=open("data.txt","w")
+# f2=open("classes.txt","w")
+#
+# n=int(input())
+# if n==1:
+#     for i in range(len(y)):
+#         f1.write(f'{x[i][0]} {x[i][1]} \n')
+#         f2.write(f'{y[i]} \n')
+# f1.close()
+# f2.close()
+
 neighbours = 5
 
-f = open('data.txt')
-for i in range(0, 10 ** 5):
-    xcoord, ycoord, n = list(map(int, f.readline().split()))
-    arr.append((xcoord, ycoord))
-    dataset.update({arr[i]: n})
+f1 = open('data.txt')
+f2 = open('classes.txt')
+coords = np.array(list(map(float, f1.read().split())))
+dots = np.reshape(coords, (-1, 2))
+classes = np.array(list(map(int, f2.readlines())))
+f1.close()
+f2.close()
 
-# print(dataset.values())
+dots_train, dots_test, class_train, class_test = train_test_split(dots, classes, train_size=0.25)
 
-f.close()
-
-dots_train, dots_test, class_train, class_test = train_test_split(list(dataset.keys()), list(dataset.values()),
-                                                                  train_size=0.7)
-
-# print(arr_train,'\n',arr_test)
+fig0 = plt.figure(0)
+fig0.suptitle('Train data')
+plt.scatter(dots_train[:, 0], dots_train[:, 1], c=class_train, s=0.2)
+plt.scatter(dots_test[:, 0], dots_test[:, 1], c='black', s=0.2)
 
 knn = KNeighborsClassifier(n_neighbors=neighbours)
 knn_model = knn.fit(dots_train, class_train)
 knn_predictions = knn.predict(dots_test)
-# print(class_test)
-# print(knn_predictions)
 accuracy = accuracy_score(class_test, knn_predictions)
 print(f'Accuracy: {accuracy}')
 
-x = [[], [], [], [], [], [], [], []]
-y = [[], [], [], [], [], [], [], []]
-for j in range(1, 7):
-    for i in range(0, len(dots_test)):
-        if knn_predictions[i] == j:
-            x[j].append(dots_test[i][0])
-            y[j].append(dots_test[i][1])
-plt.scatter(x[1], y[1], s=0.5, color='pink')
-plt.scatter(x[2], y[2], s=0.5, color='red')
-plt.scatter(x[3], y[3], s=0.5, color='orange')
-plt.scatter(x[4], y[4], s=0.5, color='purple')
-plt.scatter(x[5], y[5], s=0.5, color='magenta')
-plt.scatter(x[6], y[6], s=0.5, color='aquamarine')
+fig1 = plt.figure(1)
+fig1.suptitle('Classes')
+plt.scatter(dots[:, 0], dots[:, 1], c=classes, s=0.2)
 
-plt.plot([-10 ** 5, 10 ** 5], [0, 0], linewidth=0.25, color='black')
-plt.plot([-l / 3, -l / 3], [-10 ** 5, 10 ** 5], linewidth=0.25, color='black')
-plt.plot([l / 3, l / 3], [-10 ** 5, 10 ** 5], linewidth=0.25, color='black')
-
+fig2 = plt.figure(2)
+fig2.suptitle('Results of classification')
+plt.scatter(dots_train[:, 0], dots_train[:, 1], c=class_train, s=0.2)
+plt.scatter(dots_test[:, 0], dots_test[:, 1], c=knn_predictions, s=0.2)
 plt.show()
--- 2023.04.23homework5/SVM.py
+++ 2023.04.23homework5/SVM.py
@@ -5,60 +5,49 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-l = 10 ** 5
-arr = []
-x = []
-y = []
-dataset = {}
+def cart2pol(x, y):
+    rho = np.sqrt(x ** 2 + y ** 2)
+    phi = np.arctan2(y, x)
+    return (rho, phi)
 
-f = open('data.txt')
-for i in range(0, 10 ** 5):
-    x1, y1, n = list(map(int, f.readline().split()))
-    arr.append((x1, y1))
-    dataset.update({arr[i]: n})
 
-f.close()
-# for i in range(0, 10 ** 2):
-#     x1 = random.randint(-l, l)
-#     y1 = random.randint(-l, l)
-#     x.append(x1)
-#     y.append(y1)
-#     arr.append((x1, y1))
-#     if(y1>=0):
-#         if(x1<=(-l/3)): dataset.update({arr[i]:1})
-#         if((-l/3<x1<=l/3)): dataset.update({arr[i]:2})
-#         if (x1 > (l / 3)): dataset.update({arr[i]: 3})
-#     else:
-#         if (x1 <= (-l / 3)): dataset.update({arr[i]: 4})
-#         if ((-l / 3 < x1 <= l / 3)): dataset.update({arr[i]: 5})
-#         if (x1 > (l / 3)): dataset.update({arr[i]: 6})
-# print(dataset.values())
+def pol2cart(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return (x, y)
 
-dots_train, dots_test, class_train, class_test = train_test_split(list(dataset.keys()), list(dataset.values()),
-                                                                  train_size=0.7)
+f1 = open('data.txt')
+f2 = open('classes.txt')
+coords = np.array(list(map(float, f1.read().split())))
+dots = np.reshape(coords, (-1, 2))
+classes = np.array(list(map(int, f2.readlines())))
+f1.close()
+f2.close()
 
-model = svm.SVC()
+# for i in range(len(dots)):
+#     dots[i] = np.array(cart2pol(dots[i, 0], dots[i, 1]))
+
+dots_train, dots_test, class_train, class_test = train_test_split(dots, classes, train_size=0.25)
+
+model = svm.SVC(kernel='linear')
 svm_model = model.fit(dots_train, class_train)
 svm_predictions = svm_model.predict(dots_test)
 accuracy = accuracy_score(class_test, svm_predictions)
 print(f'Accuracy: {accuracy}')
 
-x = [[], [], [], [], [], [], [], []]
-y = [[], [], [], [], [], [], [], []]
-for j in range(1, 7):
-    for i in range(0, len(dots_test)):
-        if svm_predictions[i] == j:
-            x[j].append(dots_test[i][0])
-            y[j].append(dots_test[i][1])
-plt.scatter(x[1], y[1], s=0.5, color='pink')
-plt.scatter(x[2], y[2], s=0.5, color='red')
-plt.scatter(x[3], y[3], s=0.5, color='orange')
-plt.scatter(x[4], y[4], s=0.5, color='purple')
-plt.scatter(x[5], y[5], s=0.5, color='magenta')
-plt.scatter(x[6], y[6], s=0.5, color='aquamarine')
+# for i in range(len(dots)):
+#     dots[i] = np.array(pol2cart(dots[i, 0], dots[i, 1]))
+# for i in range(len(dots_train)):
+#     dots_train[i] = np.array(pol2cart(dots_train[i, 0], dots_train[i, 1]))
+# for i in range(len(dots_test)):
+#     dots_test[i] = np.array(pol2cart(dots_test[i, 0], dots_test[i, 1]))
 
-plt.plot([-10 ** 5, 10 ** 5], [0, 0], linewidth=0.25, color='black')
-plt.plot([-l / 3, -l / 3], [-10 ** 5, 10 ** 5], linewidth=0.25, color='black')
-plt.plot([l / 3, l / 3], [-10 ** 5, 10 ** 5], linewidth=0.25, color='black')
+fig1 = plt.figure(1)
+fig1.suptitle('Classes')
+plt.scatter(dots[:, 0], dots[:, 1], c=classes, s=0.2)
 
+fig2 = plt.figure(2)
+fig2.suptitle('Results of classification')
+plt.scatter(dots_train[:, 0], dots_train[:, 1], c=class_train, s=0.2)
+plt.scatter(dots_test[:, 0], dots_test[:, 1], c=svm_predictions, s=0.2)
 plt.show()
